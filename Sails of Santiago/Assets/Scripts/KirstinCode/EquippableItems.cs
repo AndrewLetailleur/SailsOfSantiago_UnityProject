ef8a57f2f9f982868cf9bea7865d8020adeee5b8
Andrew Letailleur <31889767+AndrewLetailleur@users.noreply.github.com>
2019-03-15 09:57:24 +0000

    prep for inventory hack test

---
+++ Sails of Santiago/Assets/Scripts/KirstinCode/EquippableItems.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public enum EquipmentType
+{
+    Handgun,
+    Bullet,
+    Gun, 
+    Bazooka,
+
+}
+[CreateAssetMenu]
+public class EquippableItems : Items {
+
+    public int StrengthBonus;
+    public int AgilityBonus;
+    public int IntellienceBonus;
+    public int VitalityBonus;
+    [Space]
+
+
+}
+
+
